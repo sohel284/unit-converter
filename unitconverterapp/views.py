be5a901aae90d2718f9fe4f002596fca017a712b
Rafiqunnabi Nayan <rafiqnayan@gmail.com>
2020-01-24 02:48:52 +0600

    -added converter calculation

--- unitconverterapp/views.py
+++ unitconverterapp/views.py
@@ -15,6 +15,7 @@ def home(request, converter_name=None):
     categories = Category.objects.all()
     
     if converter is not None:
+        converter.inverse_rate = (1.0 / converter.rate);
         converters = Conversion.objects.filter(
             from_unit__category=converter.from_unit.category
         ).exclude(pk=converter.pk)
